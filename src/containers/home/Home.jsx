9f4dd875840c685106117cae8bbebb6a8b758dae
ZSabakh <zsabakhtarishvili@atu.edu>
2021-02-15 15:11:53 -0600

    Added footer

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import CustomCarousel from "../../components/customCarousel/CustomCarousel.js";
 import Timeline from "../../components/timeline/Timeline.js";
+import Footer from "../../components/footer/Footer";
 
 export default function () {
   return (
     <>
       <CustomCarousel />
       <Timeline />
+      <Footer />
     </>
   );
 }
